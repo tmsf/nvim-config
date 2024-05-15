553b5d625362762dfbdf7c1d3c511f06be7930eb
Tiago Fernandes <tiago.soares@cmrad.com>
2024-05-16 00:02:44 +0200

    add my config

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = 'left',
+        width = 30,
+      },
+      filesystem = {
+        -- reveal = true,
+        -- position = 'right',
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+           '.git',
+           -- '.DS_Store',
+           -- 'thumbs.db',
+          },
+          never_show = {},
+        },
+      }
+    }
+  }
